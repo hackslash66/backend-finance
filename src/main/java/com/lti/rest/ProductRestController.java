19552b1197b1efd5f39a0ae2cea425cd861a79a1
EPHMRS <spssps99@gmail.com>
2020-10-01 14:06:47 +0530

    corrections

--- src/main/java/com/lti/rest/ProductRestController.java
+++ src/main/java/com/lti/rest/ProductRestController.java
@@ -3,11 +3,12 @@ package com.lti.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.entity.Product;
 import com.lti.service.ProductService;
@@ -18,24 +19,26 @@ import com.lti.service.ProductService;
  *
  */
 
+@CrossOrigin
+@RestController
 public class ProductRestController {
  
 	@Autowired
 	private ProductService service;
 	
-	@PostMapping(value = "/add", consumes = "application/json")
+	@PostMapping(value = "/addproduct", consumes = "application/json")
 	public String addProduct(@RequestBody Product product) {
 		service.persist(product);
 		return "Product added successfully";
 	}
 	
-	@GetMapping(value = "/fetch/{id}", produces = "application/json")
+	@GetMapping(value = "/fetchproduct/{id}", produces = "application/json")
 	public Product fetchProduct(@PathVariable int pId) {
 		return service.find(pId);
 	}
 	
-	@GetMapping(value = "/list", produces = "application/json")
-	public List<Product> listEmployee(){
+	@GetMapping(value = "/listproducts", produces = "application/json")
+	public List<Product> listProducts(){
 		return service.load();
 	}
 	
