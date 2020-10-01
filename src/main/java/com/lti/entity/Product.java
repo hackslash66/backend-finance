a9d40b7c4d73a019de32ef0487d48ffc93959d4a
EPHMRS <spssps99@gmail.com>
2020-10-01 15:55:44 +0530

    small change

--- src/main/java/com/lti/entity/Product.java
+++ src/main/java/com/lti/entity/Product.java
@@ -1,20 +1,10 @@
 package com.lti.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -40,11 +30,9 @@ public class Product {
 	@Column(name="pdetails",length = 200)
 	private String pDetails;
 	
-	@Column
+	@Column(name = "pRate", length = 10)
 	private double pRate;
-	
-	
-	
+		
 	public int getPid() {
 		return pID;
 	}
